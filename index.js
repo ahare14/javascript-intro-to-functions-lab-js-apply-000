55375cb71a2f1f4328f893d9342d480cf9ed4d05
Alexander Hare <alexanderhare14@gmail.com>
2018-10-28 16:04:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,9 @@ function logShout(string){
 function logWhisper(string){
   console.log(string.toLowerCase())
 }
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase())
+    return ("I can't hear you!")
+}
+  else if  (string === string.toUpperCase())
+  

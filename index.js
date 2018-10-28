bc9d65d7147706bd84413dead8ed473d0fd5e0f9
Alexander Hare <alexanderhare14@gmail.com>
2018-10-28 15:47:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,10 +12,17 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(string) {
   if ( string === string.toLowerCase() ) {
+<<<<<<< HEAD
     return ("I can't hear you!")
   }
   else if ( string === string.toUpperCase() ) {
     return ("YES INDEED!")
+=======
+    return "I can't hear you!"
+  }
+  else if ( string === string.toUpperCase() ) {
+    return "YES INDEED!"
+>>>>>>> 4393e7018684d9ad7fffad0528038e318395b6c4
   }
   else {
     return ("I love you, too.")
